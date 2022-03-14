7b5dbbdc89ce94457b0ac90ba82d554216ee7a4c
Muhammad Efendi <92834785+Muhammadefendi013@users.noreply.github.com>
2022-03-14 19:40:43 +0700

    Create atas.php

---
+++ Pertemuan3/atas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SASS-STTNF</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+<body >
+    <header>
+        <h2>Student Activity Score</h2>
+        <a href="#">Home</a>
+        <a href="#">Activity</a>
+        <a href="#">MyScore</a>
+        <a href="#">Login</a>
+    </header>
