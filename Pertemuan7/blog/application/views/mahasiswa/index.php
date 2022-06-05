7f9cbe8bc44ddec849664e32922eb00c2ccc408e
Muhammad Efendi <92834785+Muhammadefendi013@users.noreply.github.com>
2022-06-05 19:59:54 +0700

    Muhammadefendi013/

---
+++ Pertemuan7/blog/application/views/mahasiswa/index.php
@@ -0,0 +1,55 @@
+<div class="col-md-12 p-3">
+ <h3>Daftar Mahasiswa</h3>
+    <table border="3" class="table">
+        <script>
+            function hapusMahasiswa(pesan){
+                if (confirm(pesan)) {
+                    return true;
+                    
+                } else {
+                    return false;
+                }
+            }
+        </script>
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Gender</th>
+                <th>IPK</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+ <?php
+    $nomor=1;
+    foreach($mahasiswa as $mhs){
+ ?>
+ <tr>
+    <td><?=$nomor?></td>
+    <td><?=$mhs->nim?></td>
+    <td><?=$mhs->nama?></td>
+    <td><?=$mhs->gender?></td>
+    <td><?=$mhs->ipk?></td>
+    <td>
+        <a href="<?php echo base_url("index.php/mahasiswa/detail/$mhs->id")?>">Detail</a>
+        &nbsp;
+        <a href= <?php echo base_url("index.php/mahasiswa/edit/$mhs->id")?> 
+        class="btn btn-succes btn-lg active" >Edit</a>
+        &nbsp;
+        <a href= <?php echo base_url("index.php/mahasiswa/delete/$mhs->id")?> 
+        class="btn btn-danger btn-lg active" onclick="return hapusMahasiswa('Anda yakin mengahapus yang bernama <?php echo $mhs->nama?> ?')" >Hapus</a>
+    </td>
+</tr>
+ 
+<?php
+ $nomor++;
+ 
+}
+?>
+ </tbody>
+ </table>
+    <a href= <?php echo base_url("index.php/mahasiswa/form")?> 
+        class="btn btn-primary btn-lg active" >Tambah</a>
+</div>
---
+++ Pertemuan7/blog/application/views/matakuliah/index.php
@@ -0,0 +1,49 @@
+<div class="col-md-12 p-3">
+ <h3>Daftar Matakuliah</h3>
+    <table border="3" class="table">
+    <script>
+            function hapusMatakuliah(pesan){
+                if (confirm(pesan)) {
+                    return true;
+                    
+                } else {
+                    return false;
+                }
+            }
+        </script>
+        <thead>
+            <tr>
+                <th>Nomor</th>
+                <th>Nama</th>
+                <th>Sks</th>
+                <th>Kode</th>
+                <th>action</th>
+            </tr>
+        </thead>
+        <tbody>
+ <?php
+    $nomor=1;
+    foreach($matakuliah as $matkul){
+ ?>
+ <tr>
+    <td><?=$nomor?></td>
+    <td><?=$matkul->nama?></td>
+    <td><?=$matkul->sks?></td>
+    <td><?=$matkul->kode?></td>
+    <td>
+        <a href= <?php echo base_url("index.php/matakuliah/delete/$matkul->id")?> 
+        class="btn btn-danger btn-lg active" onclick="return hapusMatakuliah('Anda yakin mengahapus yang bernama <?php echo $matkul->nama?> ?')" >Hapus</a>
+    </td>
+</tr>
+ 
+<?php
+ $nomor++;
+ 
+}
+?>
+ </tbody>
+ </table>
+ <a href= <?php echo base_url("index.php/matakuliah/form")?> 
+        class="btn btn-primary btn-lg active" >Tambah</a>
+</div>
+</div>
